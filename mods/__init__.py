42eca8b38b5be4f4815804ca66d6b6cd68ecc8d1
Bart Thate <bthate@dds.nl>
2020-10-20 14:55:11 +0200

    up version

--- mods/__init__.py
+++ mods/__init__.py
@@ -2,6 +2,6 @@
 #
 #
 
-__version__ = 5
+__version__ = 6
 __txt__ = "The king of the netherlands commits genocide - OTP-CR-117/19/001 - otp.informationdesk@icc-cpi.int - https://genocide.rtfd.io"
 __txt2__ = "OTP-CR-117/19/001 | https://genocide.rtfd.io"

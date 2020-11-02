67ffd59e84dea0d0d95248ed3424e997ee9d5984
Bart Thate <bthate@dds.nl>
2020-11-02 14:35:18 +0100

    add triple

--- triple/version.py
+++ triple/version.py
@@ -2,10 +2,11 @@
 #
 # OTP-CR-117/19/001 otp.informationdesk@icc-cpi.int https://genocide.rtfd.io
 
-from genocide import __version__, __txt2__
+__version__ = 9
+__txt__ = "OTP-CR-117/19/001 otp.informationdesk@icc-cpi.int https://genocide.rtfd.io"
 
 from triple import krn
 
 def ver(event):
     "show version (ver)"
-    event.reply("GENOCIDE %s | TRIPLE %s | %s" % (__version__, krn.__version__, __txt2__))
+    event.reply("GENOCIDE %s | TRIPLE %s | %s" % (__version__, krn.__version__, __txt__))

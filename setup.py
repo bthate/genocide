c7405a1616dd65ba250830e91af0ca88794b513c
Bart Thate <bthate@dds.nl>
2020-11-03 00:56:08 +0100

    update

--- setup.py
+++ setup.py
@@ -4,64 +4,8 @@
 
 __copyright__= "Public Domain"
 
-import atexit, os
 
 from setuptools import setup
-from setuptools.command.install import install
-
-servicetxt="""[Unit]
-Description=GENOCIDE - the king of the netherlands commits genocide | OTP-CR-117/19/001 | otp.informationdesk@icc-cpi.| int https://genocide.rtfd.io
-After=network-online.target
-
-[Service]
-Group=genocide
-User=genocide
-StandardOutput=append:/var/log/genocide/genocide.log
-StandardError=append:/var/log/genocide/genocide.log
-ExecStart=/usr/local/bin/genocide wd=/var/lib/genocide mods=irc,rss,udp -w
-
-[Install]
-WantedBy=multi-user.target
-"""
-
-class Install(install):
-    def __init__(self, *args, **kwargs):
-        super(Install, self).__init__(*args, **kwargs)
-        atexit.register(postinstall)
-
-def skipopen(txt, skip=["already"]):
-    txt += " 2>&1"
-    try:
-        for line in os.popen(txt).readlines():
-             pass
-    except Exception as ex:
-        for rej in skip:
-           if rej in str(ex):
-               return
-
-def postinstall():
-    skipopen("mkdir /var/lib/genocide")
-    skipopen("mkdir /var/lib/genocide/mods")
-    skipopen("mkdir /var/lib/genocide/store")
-    skipopen("mkdir /var/log/genocide")
-    skipopen("touch /var/log/genocide/genocide.log")
-    skipopen("chown -R genocide:genocide /var/lib/genocide")
-    skipopen("chown -R genocide:genocide /var/log/genocide")
-    skipopen("chmod 700 /var/lib/genocide/")
-    skipopen("chmod 700 /var/lib/genocide/mods")
-    skipopen("chmod 700 /var/lib/genocide/store")
-    skipopen("chmod -R 400 /var/lib/genocide/mods/*.py")
-    skipopen("chmod 744 /var/log/genocide/")
-    skipopen("groupadd genocide")
-    skipopen("useradd genocide -g triple -d /var/lib/genocide")
-    writeservice()
-
-def writeservice():
-    p = "/etc/systemd/system/genocide.service"
-    if not os.path.exists(p):
-        f = open(p, "w")
-        f.write(servicetxt)
-        f.close()
 
 def mods():
     import os
@@ -76,14 +20,14 @@ setup(
     url='https://github.com/bthate/genocide',
     author='Bart Thate',
     author_email='bthate@dds.nl', 
-    description="GENOCIDE 9 TRIPLE 3 OTP-CR-117/19/001 otp.informationdesk@icc-cpi.int https://genocide.rtfd.io",
+    description="GENOCIDE 9 TRIPLE 3 | OTP-CR-117/19/001 | otp.informationdesk@icc-cpi.int | https://genocide.rtfd.io",
     long_description=read(),
     license='Public Domain',
     packages=["triple"],
     namespace_packages=["triple"],
     zip_safe=False,
     scripts=["bin/genocide"],
-    cmdclass={'install': Install},
+    #cmdclass={'install': Install},
     classifiers=['Development Status :: 3 - Alpha',
                  'License :: OSI Approved :: MIT License',
                  'Operating System :: Unix',

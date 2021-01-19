a2b1379cafce4566285d88604451b5709bb618ee
Bart Thate <bthate@dds.nl>
2021-01-19 02:11:32 +0100

    up version

--- genocide/ver.py
+++ genocide/ver.py
@@ -1,8 +1,8 @@
-# GENOCIDE - One decision from a judge that it is after 2002
+# GENOCIDE - OTP-CR-117/19 | otp.informationdesk@icc-cpi.int
 #
 # this file is placed in the public domain
 
-__version__ = 21
+__version__ = 22
 
 def ver(event):
-    event.reply("GENOCIDE %s OTP-CR-117/19/001 otp.informationdesk@icc-cpi.int http://genocide.rtfd.io" % __version__)
+    event.reply("GENOCIDE %s | OTP-CR-117/19 | otp.informationdesk@icc-cpi.int | http://genocide.rtfd.io" % __version__)

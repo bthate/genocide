3bd21c4366460426fa74553e0ebfc52fee2da25a
Bart Thate <bart@dds.nl>
2021-02-04 13:47:33 +0100

    over to the project page

--- genocide/ver.py
+++ genocide/ver.py
@@ -1,8 +1,6 @@
 # GENOCIDE - OTP-CR-117/19 | otp.informationdesk@icc-cpi.int
 #
-# this file is placed in the public domain
-
-__version__ = 25
+# This file is placed in the Public Domain.
 
 def ver(event):
-    event.reply("GENOCIDE %s OTP-CR-117/19 otp.informationdesk@icc-cpi.int http://genocide.rtfd.io" % __version__)
+    event.reply("OTP-CR-117/19 otp.informationdesk@icc-cpi.int http://pypi.org/project/genocide !")

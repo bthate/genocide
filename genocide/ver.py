c403e376ae271e2f1b5aaf97955279a59705a6bf
Bart Thate <bart@dds.nl>
2021-02-06 11:33:43 +0100

    use gc

--- genocide/ver.py
+++
@@ -1,8 +0,0 @@
-# GENOCIDE - OTP-CR-117/19 | otp.informationdesk@icc-cpi.int
-#
-# This file is placed in the Public Domain.
-
-__version__ = 26
-
-def ver(event):
-    event.reply("OTP-CR-117/19 otp.informationdesk@icc-cpi.int http://pypi.org/project/genocide !")

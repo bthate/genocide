070be565270b08020d3c8cd2773bd4f56833cb61
Bart Thate <bthate@dds.nl>
2021-03-05 21:32:08 +0100

    add update

---
+++ ob/run.py
@@ -0,0 +1,14 @@
+# This file is placed in the Public Domain
+
+"runtime"
+
+import os
+import sys
+
+from ob import Object, j, update, cfg
+from ob.dbs import last
+from ob.hdl import Bus
+from ob.ofn import overlay
+from ob.shl import Shell, exec
+from ob.tbl import tbl
+from ob.utl import privileges

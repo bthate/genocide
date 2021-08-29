b1a6678518c72738a2567e97063381d7afecaf43
Bart Thate <bthate67@gmail.com>
2021-08-29 19:25:15 +0200

    update to gcid

--- bot/all.py
+++
@@ -1,8 +0,0 @@
-import bot.adm
-import bot.cms
-import bot.fnd
-import bot.irc
-import bot.log
-import bot.rss
-import bot.tdo
-import bot.udp
--- genocide/ver.py
+++ genocide/ver.py
@@ -1,8 +1,1 @@
-# This file is in the Public Doman.
-
-"genocide version"
-
-__version__ = 38
-
-def ver(event):
-    event.reply("GENOCIDE %s - http://pypi.org/project/genocide otp.informationdesk@icc-cpi.int  OTP-CR-117/19" % __version__)
+__version__ = 124

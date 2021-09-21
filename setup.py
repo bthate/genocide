4fd6844a8cc7f36f3f60100d5e6561cf51a3c97a
Bart Thate <bthate67@gmail.com>
2021-09-21 15:58:02 +0200

    move man page to man.rst

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def uploadlist(dir):
 
 setup(
     name='genocide',
-    version='40',
+    version='41',
     url='https://github.com/bthate/genocide',
     author='Bart Thate',
     author_email='bthate67@gmail.com', 

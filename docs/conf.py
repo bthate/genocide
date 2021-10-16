b677040c3d8490320af4b52ef2d6dd147896d3e3
Bart Thate <bthate67@gmail.com>
2021-10-16 14:51:43 +0200

    update

--- docs/conf.py
+++ docs/conf.py
@@ -61,7 +61,7 @@ html_use_opensearch = 'http://genocide.rtfd.io/'
 html_file_suffix = '.html'
 htmlhelp_basename = 'testdoc'
 
-rst_prolog = """.. image:: genocide3line.png
+rst_prolog = """.. image:: clozastrip.jpg
     :width: 100%
 
 .. title:: PROSECUTE KING NETHERLANDS FOR GENOCIDE

e0419d743e96b30fea3c1f0fcd2c07ba5a541fff
Bart Thate <bthate67@gmail.com>
2022-05-10 21:29:02 +0200

    fix cmds

--- genocide/cmds.py
+++ genocide/cmds.py
@@ -10,7 +10,7 @@ import time
 
 from hdl import Bus, Commands, starttime
 from irc import Config
-from obj import Class, Db, Object, edit, find, format, get, last, save, update
+from obj import Class, Db, Object, edit, find, fntime, format, get, last, save, update
 from rpt import elapsed
 from thr import getname
 
@@ -32,107 +32,6 @@ def cmd(event):
 Commands.add(cmd)
 
 
-def flt(event):
-    try:
-        index = int(event.args[0])
-        event.reply(Bus.objs[index])
-        return
-    except (KeyError, TypeError, IndexError, ValueError):
-        pass
-    event.reply(" | ".join([getname(o) for o in Bus.objs]))
-
-
-Commands.add(flt)
-
-
-def fnd(event):
-    if not event.args:
-        db = Db()
-        res = ",".join(
-            sorted({x.split(".")[-1].lower() for x in db.types()}))
-        if res:
-            event.reply(res)
-        else:
-            event.reply("no types yet.")
-        return
-    bot = event.bot()
-    otype = event.args[0]
-    res = list(find(otype))
-    if bot.cache:
-         if len(res) > 3:
-             bot.extend(event.channel, [x[1].txt for x in res])
-             bot.say(event.channel, "%s left in cache, use !mre to show more" % bot.cache.size())
-             return
-    nr = 0
-    for _fn, o in res:
-        txt = "%s %s" % (str(nr), format(o))
-        nr += 1
-        event.reply(txt)
-    if nr:
-        event.reply("no result")
-
-
-Commands.add(fnd)
-
-
-def log(event):
-    if not event.rest:
-        event.reply("log <txt>")
-        return
-    o = Log()
-    o.txt = event.rest
-    save(o)
-    event.reply("ok")
-
-
-Commands.add(log)
-
-
-def thr(event):
-    result = []
-    for t in sorted(threading.enumerate(), key=lambda x: x.getName()):
-        if str(t).startswith("<_"):
-            continue
-        o = Object()
-        update(o, vars(t))
-        if get(o, "sleep", None):
-            up = o.sleep - int(time.time() - o.state.latest)
-        else:
-            up = int(time.time() - starttime)
-        result.append((up, t.getName()))
-    res = []
-    for up, txt in sorted(result, key=lambda x: x[0]):
-        res.append("%s(%s)" % (txt, elapsed(up)))
-    if res:
-        event.reply(" ".join(res))
-
-
-Commands.add(thr)
-
-
-# This file is placed in the Public Domain.
-
-
-"commands"
-
-
-import threading
-import time
-
-
-from hdl import Bus, Commands, starttime
-from obj import Class, Db, Object, edit, find, fntime, format, get, last, save, update
-from rpt import elapsed
-from thr import getname
-
-
-def cmd(event):
-    event.reply(",".join(sorted(Commands.cmd)))
-
-
-Commands.add(cmd)
-
-
 def dlt(event):
     if not event.args:
         event.reply("dlt <username>")
@@ -145,6 +44,24 @@ def dlt(event):
         break
 
 Commands.add(dlt)
+
+
+def dpl(event):
+    if len(event.args) < 2:
+        event.reply("dpl <stringinurl> <item1,item2>")
+        return
+    db = Db()
+    setter = {"display_list": event.args[1]}
+    names = Class.full("rss")
+    if names:
+        _fn, o = db.lastmatch(names[0], {"rss": event.args[0]})
+        if o:
+            edit(o, setter)
+            save(o)
+            event.reply("ok")
+
+
+Commands.add(dpl)
 
 
 def flt(event):
@@ -190,63 +107,6 @@ def fnd(event):
 Commands.add(fnd)
 
 
-def met(event):
-    if not event.args:
-        event.reply("met <userhost>")
-        return
-    user = User()
-    user.user = event.rest
-    user.perms = ["USER"]
-    save(user)
-    event.reply("ok")
-
-
-Commands.add(met)
-
-
-def thr(event):
-    result = []
-    for t in sorted(threading.enumerate(), key=lambda x: x.getName()):
-        if str(t).startswith("<_"):
-            continue
-        o = Object()
-        update(o, vars(t))
-        if get(o, "sleep", None):
-            up = o.sleep - int(time.time() - o.state.latest)
-        else:
-            up = int(time.time() - starttime)
-        result.append((up, t.getName()))
-    res = []
-    for up, txt in sorted(result, key=lambda x: x[0]):
-        res.append("%s(%s)" % (txt, elapsed(up)))
-    if res:
-        event.reply(" ".join(res))
-
-
-Commands.add(thr)
-
-
-## rss
-
-
-def dpl(event):
-    if len(event.args) < 2:
-        event.reply("dpl <stringinurl> <item1,item2>")
-        return
-    db = Db()
-    setter = {"display_list": event.args[1]}
-    names = Class.full("rss")
-    if names:
-        _fn, o = db.lastmatch(names[0], {"rss": event.args[0]})
-        if o:
-            edit(o, setter)
-            save(o)
-            event.reply("ok")
-
-
-Commands.add(dpl)
-
-
 def ftc(event):
     res = []
     thrs = []
@@ -261,6 +121,33 @@ def ftc(event):
 
 
 Commands.add(ftc)
+
+
+def met(event):
+    if not event.args:
+        event.reply("met <userhost>")
+        return
+    user = User()
+    user.user = event.rest
+    user.perms = ["USER"]
+    save(user)
+    event.reply("ok")
+
+
+Commands.add(met)
+
+
+def log(event):
+    if not event.rest:
+        event.reply("log <txt>")
+        return
+    o = Log()
+    o.txt = event.rest
+    save(o)
+    event.reply("ok")
+
+
+Commands.add(log)
 
 
 def nme(event):
@@ -280,7 +167,6 @@ def nme(event):
 
 
 Commands.add(nme)
-
 
 def rem(event):
     if not event.args:
@@ -319,3 +205,24 @@ def rss(event):
 
 
 Commands.add(rss)
+
+def thr(event):
+    result = []
+    for t in sorted(threading.enumerate(), key=lambda x: x.getName()):
+        if str(t).startswith("<_"):
+            continue
+        o = Object()
+        update(o, vars(t))
+        if get(o, "sleep", None):
+            up = o.sleep - int(time.time() - o.state.latest)
+        else:
+            up = int(time.time() - starttime)
+        result.append((up, t.getName()))
+    res = []
+    for up, txt in sorted(result, key=lambda x: x[0]):
+        res.append("%s(%s)" % (txt, elapsed(up)))
+    if res:
+        event.reply(" ".join(res))
+
+
+Commands.add(thr)

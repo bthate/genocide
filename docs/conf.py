750cb846263b75c910fe34f098655bde24050ec5
Bart Thate <bthate67@gmail.com>
2022-05-12 00:14:35 +0200

    update banner

--- docs/conf.py
+++ docs/conf.py
@@ -119,7 +119,7 @@ rst_prolog = '''.. image:: line.png
 
     <br>
 
- <center><i>Elderly and handicapped (Wzd), psychiatric patients (WvGGZ) and criminals (Wfz)</i></center>
+ <center><i>elderly and handicapped (Wzd), psychiatric patients (WvGGZ) and criminals (Wfz)</i></center>
 
 '''
 

754d16227c57d5194314c40d804869ebd6b0123f
Bart Thate <bthate67@gmail.com>
2022-05-11 15:17:30 +0200

    fixors

--- genocide/all.py
+++ genocide/all.py
@@ -1,8 +1,12 @@
 # This file is placed in the Public Domain.
 
 
+import genocide
+
+
 def __dir__():
     return (
+        "cmds",
         "mdl",
         "req",
         "slg",
@@ -15,12 +19,13 @@ def __dir__():
 from tbl import Table
 
 
-from genocide import mdl
-from genocide import req
-from genocide import slg
-from genocide import sui
-from genocide import trt
-from genocide import wsd
+from genocide import mdl as mdl
+from genocide import req as req
+from genocide import slg as slg
+from genocide import sui as sui
+from genocide import trt as trt
+from genocide import wsd as wsd
+from genocide import cmds as cmds
 
 
 for mn in __dir__():

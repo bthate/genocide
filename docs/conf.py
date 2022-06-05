aff89cc8d8ede573d7a33e9f9210835d649766c1
Bart Thate <bthate67@gmail.com>
2022-06-05 17:22:17 +0200

    update config

--- docs/conf.py
+++ docs/conf.py
@@ -114,7 +114,7 @@ rst_prolog = '''.. image:: line.png
 
     <center>
 
-:ref:`reconsider <reconsider>` - :ref:`request <request>` - :ref:`evidence <evidence>` - :ref:`guilty <guilty>` - :ref:`writings <writings>` - :ref:`source <source>` - :ref:`manual <man>`
+:ref:`reconsider <reconsider>` - :ref:`request <request>` - :ref:`evidence <evidence>` - :ref:`guilty <guilty>` - :ref:`writings <writings>` 
 
 .. raw:: html
 

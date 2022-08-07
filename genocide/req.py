9f70e5401c3228c070ad8fb7bd1d5d76f795d706
Bart Thate <bthate67@gmail.com>
2022-08-07 21:48:10 +0200

    update request

--- genocide/req.py
+++ genocide/req.py
@@ -1,11 +1,11 @@
 # This file is placed in the Public Domain.
 
 
-"""| Information and Evidence Unit
-| Office of the Prosecutor
-| Post Office Box 19519
-| 2500 CM The Hague
-| The Netherlands
+"""| **Information and Evidence Unit**
+| **Office of the Prosecutor**
+| **Post Office Box 19519**
+| **2500 CM The Hague**
+| **The Netherlands**
 
 Hello Office of the Prosecutor,
 

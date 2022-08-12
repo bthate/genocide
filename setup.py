ed71231795a2a9908a2d79611d3dde2fbef255c0
Bart Thate <bthate67@gmail.com>
2022-08-12 15:19:25 +0200

    up version

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def uploadlist(dir):
 
 setup(
     name='genocide',
-    version='75',
+    version='80',
     url='https://github.com/bthate/genocide',
     author='Bart Thate',
     author_email='bthate67@gmail.com', 

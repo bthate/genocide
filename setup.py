2523ee15a2c4f7e47bd4cd554ba6372459d9d956
Bart Thate <bthate67@gmail.com>
2022-08-14 09:39:33 +0200

    up version

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     url='https://github.com/bthate/genocide',
     author='Bart Thate',
     author_email='bthate67@gmail.com', 
-    description="Prosecutor. Court. Reonsider OTP-CR-117/19.",
+    description="Prosecutor. Court. Reconsider OTP-CR-117/19.",
     long_description=read(),
     license='Public Domain',
     packages=["genocide"],

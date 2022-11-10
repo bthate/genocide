30fe94ac8479bc1535045e23dedebd5e30e676d6
Bart Thate <bthate67@gmail.com>
2022-11-10 13:54:57 +0100

    update obj

--- genocide/obj.py
+++ genocide/obj.py
@@ -2,7 +2,48 @@
 # pylint: disable=R,C,W,C0302
 
 
-"object"
+"""Big Object is a load/saveable python object
+
+This module contains a big Object class that provides a clean, no methods,
+namespace for json data to be read into. This is necessary so that methods
+don't get overwritten by __dict__ updating and, without methods defined on
+the object, is easily being updated from a on disk stored json (dict).
+
+basic usage is this::
+
+ >>> import opl
+ >>> o = opl.Object()
+ >>> o.key = "value"
+ >>> o.key
+ 'value'
+
+Some hidden methods are provided, methods are factored out into functions
+like get, items, keys, register, set, update and values.
+
+load/save from/to disk::
+
+ >>> import opl
+ >>> o = opl.Object()
+ >>> o.key = "value"
+ >>> p = opl.save(o)
+ >>> oo = opl.Object()
+ >>> opl.load(oo, p)
+ >>> oo.key
+ 'value'
+
+Big Objects can be searched with database functions and uses read-only files
+to improve persistence and a type in filename for reconstruction::
+
+ 'opl.obj.Object/2021-08-31/15:31:05.717063'
+
+ >>> import opl
+ >>> o = opl.Object()
+ >>> opl.save(o)  # doctest: +ELLIPSIS
+ 'opl.obj.Object/...'
+
+Great for giving objects peristence by having their state stored in files.
+
+"""
 
 
 ## import
@@ -70,7 +111,26 @@ __all__ = __dir__()
 
 class Object:
 
+    """Big Objects load/save themselves to/from disk.
+
+       It has no methods, it's __dict__ is clean on start (clean namespace).
+       Method are implemented as functions with the object as the first
+       argument, a trick to mimic object method calls.
+
+       >>> import opl
+       >>> o = opl.Object()
+       >>> o.test = "try"
+       >>> opl.format(o)
+       'test=try'
+
+       Some hidden methods are provided, methods are factored out into functions
+       like get, items, keys, register, set, update and values.
+
+    """
+
+
     __slots__ = ("__dict__", "__fnm__")
+
 
     def __init__(self, *args, **kwargs):
         object.__init__(self)

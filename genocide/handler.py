287efa08f3667607f2dca22d9ed953672387d82b
Bart Thate <operbot100@gmail.com>
2022-11-19 21:37:36 +0100

    use killmode=mixed

--- genocide/handler.py
+++ genocide/handler.py
@@ -80,12 +80,7 @@ class Callback(Object):
         if not func:
             event.ready()
             return
-        try:
-            func(event)
-            #event.__thr__ = launch(func, event)
-        except Exception as ex:
-            Callback.errors.append(ex)
-            event.ready()
+        func(event)
 
     def dispatch(self, event):
         self.callback(event)

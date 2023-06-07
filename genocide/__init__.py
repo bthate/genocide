1ce0c4e6ec1d5ad34120683533d4ccf5fd178263
Bart Thate <bthate@dds.nl>
2023-06-07 15:08:57 +0200

    update docstring

--- genocide/__init__.py
+++ genocide/__init__.py
@@ -3,23 +3,7 @@
 # pylama: ignore=E402,W0611,W0401,C901
 
 
-"""object programming runtime
-
-
-OPR is runtime kit that uses object programming for it's implementation.
-Object programming uses classes with the methods factored out into
-functions, to get a clean namespace to load json into and not overwrite
-any methods. A clean namespace way of programming, so to speak.
-
-OPR provides object persistence, an event handler and some basic code to
-load modules that can provide additional commands.
-
-OPR has some functionality, mostly feeding RSS feeds into a irc
-channel. It can do some logging of txt and take note of things todo.
-
-OPR is placed in the Public Domain.
-
-"""
+"Reconsider OTP-CR-117/19"
 
 
 from . import clients, clocked, command, configs, decoder, default, defines

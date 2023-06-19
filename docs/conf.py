30631a0dde3c170d1b783a90a29c0035e4a3a1c2
Bart Thate <bthate67@gmail.com>
2023-06-19 22:11:07 +0200

    use banner

--- docs/conf.py
+++ docs/conf.py
@@ -116,8 +116,26 @@ rst_prolog = '''.. image:: genocide.png
 
 .. raw:: html
 
-    <br>
+    <br><br>
 
+.. raw:: html
+
+    <center>
+    <i>
+    By law, with the use of poison, killing, torturing, castrating, destroying,
+    </i>
+    </center>
+    <center>
+    <i>
+    in whole or in part, all elderly and all handicapped (Wzd), all criminals (Wfz)
+    </i>
+    </center>
+    <center>
+    <i>
+    and all psychiatric patients (WvGGZ) here in the Netherlands
+    </i>
+    </center>
+    <br>
 '''
 
 

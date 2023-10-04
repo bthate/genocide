8dfc8824651c547839e89a48e4fd70f04113f9fa
Zelf <tehzelf@gmail.com>
2023-10-04 15:52:17 +0200

    nit

--- genocide/__main__.py
+++ genocide/__main__.py
@@ -2,7 +2,7 @@
 # This file is placed in the Public Domain.
 #
 # pylint: disable=C0412,C0115,C0116,W0212,R0903,C0207,C0413,W0611
-# pylint: disable=C0411,E0402
+# pylint: disable=C0411,E0402,E0611
 
 "runtime"
 
